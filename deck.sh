6b493f9669fecbc1533e90bf8fe0beac60c064f7
rockcutto <rockcat2015@yandex.ru>
2025-06-26 10:42:30 +0300

    Update deck.sh

--- deck.sh
+++ deck.sh
@@ -1,2 +1,9 @@
-source /skydeck_env/bin/activate
-python /skydeck_deck/deck_ctrl.py
+[Desktop Entry]
+Type=Application
+Name=SkyDeck Joystick Sender
+Comment=Автозапуск Python-скрипта deck.sh для передачи CRSF на ESP32
+Exec=bash -c "cd /home/deck/SkyDeck && ./deck.sh"
+Path=/home/deck/SkyDeck
+Terminal=true
+X-KDE-AutostartNotify=false
+OnlyShowIn=KDE;
